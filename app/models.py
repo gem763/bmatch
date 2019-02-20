c2eb87f403d1be3e0f8cf124f5ef8b538141a7c1
gem763 <gem763@hanmail.net>
2019-02-20 21:03:58 +0900

    .

--- app/models.py
+++ app/models.py
@@ -14,6 +14,7 @@ class Brand(models.Model):
     logo = models.ImageField(blank=True)
     identity = models.TextField(blank=True, null=True)
     cluster = models.CharField(max_length=50, blank=True, null=True)
+    wordcloud = models.ImageField(blank=True)
 
     def __str__(self):
         return self.name
