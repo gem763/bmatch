a204fda61d3b48d44415d7cbfb05b7a78e13b1df
sejinkang <gem763@hanmail.net>
2019-03-06 21:52:25 +0900

    .

--- app/static/js/wordcloud.js
+++ app/static/js/wordcloud.js
@@ -11,8 +11,7 @@ function WordCloud(options) {
   // set the ranges for the scales
   var xScale = d3.scaleLinear().range([10, 100]);
 
-  var focus = svg.append('g')
-                 .attr("transform", "translate(" + [w/2, h/2] + ")")
+  var focus = svg.append('g').attr("transform", "translate(" + [w/2, h/2] + ")")
 
   // var colorMap = ['red', '#a38b07'];
   var fill = d3.scaleOrdinal(d3.schemeCategory10);
@@ -38,18 +37,22 @@ function WordCloud(options) {
              .text(function(d) { return d.key; })
              .font("Impact")
              .random(arng)
-             .on("end", function(output) {
-               // sometimes the word cloud can't fit all the words- then redraw
-               // https://github.com/jasondavies/d3-cloud/issues/36
-               if (word_entries.length !== output.length) {
-                 console.log("not all words included- recreating");
-                 makeCloud();
-                 return undefined;
-               } else { draw(output); }
-             })
+             .on("end", draw)
+             // .on("end", function(output) {
+             //   // sometimes the word cloud can't fit all the words- then redraw
+             //   // https://github.com/jasondavies/d3-cloud/issues/36
+             //   if (word_entries.length !== output.length) {
+             //     console.log("not all words included- recreating");
+             //     makeCloud();
+             //     return undefined;
+             //   } else { draw(output); }
+             // })
              .start();
+
+    d3.layout.cloud().stop();
   };
-  d3.layout.cloud().stop();
+
+
 
 
   function draw(words) {
@@ -67,39 +70,51 @@ function WordCloud(options) {
            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
          })
          .text(function(d) { return d.key; })
+         .on("click", handleClick)
          //.on('mouseover', handleMouseOver)
          //.on('mouseout', handleMouseOut);
   };
 
-  // function handleMouseOver(d) {
-  //   var group = focus.append('g').attr('id', 'story-titles');
-  //    var base = d.y - d.size;
-  //
-  //   group.selectAll('text')
-  //        .data(["testtesttest"])
-  //        .enter().append('text')
-  //        .attr('x', d.x)
-  //        .attr('y', function(title, i) {
-  //          return (base - i*14);
-  //        })
-  //        .attr('text-anchor', 'middle')
-  //        .text(function(title) { return title; });
-  //
-  //   var bbox = group.node().getBBox();
-  //   var bboxPadding = 5;
-  //
-  //   // place a white background to see text more clearly
-  //   var rect = group.insert('rect', ':first-child')
-  //                 .attr('x', bbox.x)
-  //                 .attr('y', bbox.y)
-  //                 .attr('width', bbox.width + bboxPadding)
-  //                 .attr('height', bbox.height + bboxPadding)
-  //                 .attr('rx', 10)
-  //                 .attr('ry', 10)
-  //                 .attr('class', 'label-background-strong');
-  // };
-  //
-  // function handleMouseOut(d) {
-  //   d3.select('#story-titles').remove();
-  // };
+
+  function handleClick(d) {
+    d3.select('#story-titles').remove();
+
+    var item = d.key
+    var group = focus.append('g').attr('id', 'story-titles');
+    var base = d.y - d.size;
+
+    group.selectAll('text')
+         .data(["검색"])
+         .enter().append('text')
+         .attr('x', d.x)
+         .attr('y', function(title, i) {
+           return (base - i*14);
+         })
+         .attr('text-anchor', 'middle')
+         .style('fill', 'white')
+         .text(function(title) { return title; })
+         .on('click', function(d) { window.open("https://www.google.com/search?q=" + item); });
+
+    var bbox = group.node().getBBox();
+    var bboxPadding = 10;
+
+    // place a white background to see text more clearly
+    var rect = group.insert('div', ':first-child')
+                    .attr('class', 'ui button')
+                    .text('test')
+
+    // var rect = group.insert('rect', ':first-child')
+    //               .attr('x', bbox.x - bboxPadding)
+    //               .attr('y', bbox.y - bboxPadding)
+    //               .attr('width', bbox.width + 2*bboxPadding)
+    //               .attr('height', bbox.height + 2*bboxPadding)
+    //               .attr('rx', 30)
+    //               .attr('ry', 30)
+    //               .attr('class', 'label-background-strong')
+    //               ;
+  };
+
+  function handleMouseOut(d) {
+    d3.select('#story-titles').remove();
+  };
 }
